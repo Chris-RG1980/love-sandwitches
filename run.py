c817b9fa8f55301270177ade73e75c4c71b65110
Chris Grabham <chrisgrabham@outlook.com>
2023-11-06 19:05:56 +0000

    update stock values on stock worksheet

--- run.py
+++ run.py
@@ -112,6 +112,16 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    headings = SHEET.worksheet("stock").get_all_values()[0]
+
+    stock_dictionary = dict.fromkeys(headings, 0)
+
+    for i in range(len(headings)):
+        stock_dictionary[headings[i]] = data[i]
+
+    return stock_dictionary
+
 def main():
     """
     Run all program functions
@@ -124,6 +134,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
